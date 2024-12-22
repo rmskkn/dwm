e26360152e802641db175faf6780942253b118f0
Roman Skakun <rm.skakun@gmail.com>
2024-12-22 16:08:51 +0000

    config: added swallow for mpv
    Signed-off-by: Roman Skakun <rm.skakun@gmail.com>

--- config.h
+++ config.h
@@ -42,10 +42,12 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class            instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "St",             NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "Gimp",           NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Firefox",        NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "mpv",            NULL,     NULL,           0,         0,          0,           0,        -1 },
 	{ "Pavucontrol",    NULL,     NULL,           0,         1,          0,           1,        -1 },
-	{ "PrusaSlicer",    NULL,     NULL,           0,         0,          0,           1         -1 },
+	{ "PrusaSlicer",    NULL,     NULL,           0,         0,          0,           0         -1 },
 	{ NULL,             NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
